01279008e347625108a78c18821341cd32bd5db8
grep-safi <hub.of.safi@gmail.com>
2020-06-15 12:52:05 -0700

    adjusted sizing and positions

--- src/MainView.jsx
+++ src/MainView.jsx
@@ -33,10 +33,11 @@ export default class MainView extends React.Component {
                 <svg width={WIDTH} height={HEIGHT}>
                     <rect width={"100%"} height={"100%"} fill={"black"} />
                     <g>
-                        <image x={110} y={160} href={"./img/distance-line.png"} height={260} width={260} />
+                        <image x={120} y={160} href={"./img/distance-line.png"} height={250} width={250} />
+                        <text x={190} y={320} fill={"aqua"}>3000 Light Years</text>
                         <image x={100} y={160} href={"./img/star.png"} height={50} width={50} />
                         <image x={250} y={25} href={"./img/puzzled-man.jpg"} height={250} width={250} />
-                        <image x={345} y={50} href={"./img/star.png"} height={50} width={50} />
+                        <image x={345} y={48} href={"./img/star.png"} height={50} width={50} />
                         <circle cx={125} cy={186} r={0} stroke={"red"} fill={"none"} />
                     </g>
                 </svg>
